7afc6a6fa67811380d526d713003ece3ba69f749
Karthic Rao <kartronics85@gmail.com>
2020-10-03 09:33:27 +0000

    Added support for more languages

--- color_codes.js
+++ color_codes.js
@@ -19,21 +19,37 @@ let THEMES_PATH='/home/suvij/color-code/test-cases/themes/'
 app.use(express.json());
 
 const langExtensions = {
-	"java":".java",
-	"python":".py",
-	"c++":".cpp",
+	"batch":".bat",
 	"c":".c",
-	"javascript":".js",
-	"html":".html",
+	"c++":".cpp",
+	"clojure":".clj",
 	"css":".css",
+	"dockerfile":".dockerfile",
 	"go":".go",
+	"html":".html",
+	"jade":".jade",
+	"java":".java",
+	"javascript":".js",
+	"javascriptreact":".jsx",
 	"json":".json",
+	"markdown":".md",
+	"objective-c":".m",
+	"perl":".pl",
 	"php":".php",
+	"powershell":".ps1",
+	"properties":".properties",
+	"python":".py",
 	"r":".r",
 	"ruby":".rb",
 	"rust":".rs",
+	"scss":".scss",
+	"shellscript":".sh",
+	"sql":".sql",
 	"swift":".swift",
 	"typescript":".ts",
+	"typescriptreact":".tsx",
+	"xml":".xml",
+	"yaml":".yaml"
 }
 
 const getTheme = {
